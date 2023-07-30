7d60b296080461231f21a61382bdafae61536508
Gomname1356 <66259603+Aramesh1977@users.noreply.github.com>
2023-07-30 17:46:58 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "";0xdedefad813beada12540943622565ddfe2eedbf2
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
